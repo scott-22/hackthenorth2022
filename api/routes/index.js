6e05eda141d3d092a06676ca5b76431965104a5d
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-09-17 01:09:31 -0400

    Add dotenv to backend

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({
+    message: "Hello! You did a GET request, and this is the response."
+  });
+});
+
+router.post('/', (req, res) => {
+  res.send({
+    message: "Hello! You did a POST request, and this is the response."
+  });
 });
 
 module.exports = router;
