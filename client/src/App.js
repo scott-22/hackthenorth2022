e9049f3ef847f40bceb4071c3a8fd333b8412d97
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-09-17 15:22:53 -0400

    Remove chart.js, downgrade react-scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,7 @@
+import React from 'react';
+
 import './App.css';
 import { Line } from 'react-chartjs-2';
-
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js'
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
 
 function App() {
   const data = [
