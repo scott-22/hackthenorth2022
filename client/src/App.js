b2c0b96de3a7e1b84771c862191fc12916ff2ca3
Ching Lam Lau <contact.minicube11@gmail.com>
2022-09-17 09:42:38 -0400

    Line graph for time vs speed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,59 @@
 import './App.css';
+import { Line } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 function App() {
+  const data = [
+    {time: 0.17, speed: 10},
+    {time: 3.17, speed: 14},
+    {time: 6.20, speed: 5},
+    {time: 9.15, speed: 7},
+    {time: 12.15, speed: 8},
+  ];
+
+  const times = [];
+  const speeds = [];
+
+  for (const item of data) {
+    times.push(item.time);
+    speeds.push(item.speed);
+  }
+
+  const graphData = {
+    labels: times,
+    datasets: [
+      {
+        label: "speeds",
+        data: speeds,
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      },
+    ]
+  };
+
+  console.log(times, speeds);
+
   return (
     <div class="html-body">
 
@@ -10,8 +63,12 @@ function App() {
         <button class="header-text" onClick="document.getElementById('portfolio-div').scrollIntoView();">About Us</button>
         <button class="header-text" onClick="document.getElementById('portfolio-div').scrollIntoView();">Contacts</button>
       </div>
-
+      <div>
+        <h2>Data</h2>
+        <Line data={graphData} />
+      </div>
     </div>
+    
   );
 }
 
