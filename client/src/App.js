d2264e1e7313f4f436ad9e03b988511434dfac41
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-09-17 15:55:04 -0400

    Fetch velocities from backend!

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
 
 function App() {
-  const data = [
-    {time: 0.17, speed: 10},
-    {time: 3.17, speed: 14},
-    {time: 6.20, speed: 5},
-    {time: 9.15, speed: 7},
-    {time: 12.15, speed: 8},
-  ];
-
-  const times = [];
-  const speeds = [];
-  let maxSpeed = 0;
-  let averageSpeed = 0;
-  let count = 0;
-
-  for (const item of data) {
-    times.push(item.time);
-    speeds.push(item.speed);
-    maxSpeed = Math.max(maxSpeed, item.speed);
-    averageSpeed = (averageSpeed * count + item.speed) / (count + 1);
-    count++;
-  }
-
-
-
-  const graphData = {
-    labels: times,
+  const [data, setData] = useState([]);
+  const [maxSpeed, setMaxSpeed] = useState(0);
+  const [averageSpeed, setAverageSpeed] = useState(0);
+  const [graphData, setGraphData] = useState({
+    labels: [0, 1, 2],
     datasets: [
       {
         label: "speeds",
-        data: speeds,
+        data: [0, 1, 2],
         fill: true,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(75,192,192,1)"
       },
     ]
-  };
+  });
 
-  console.log(times, speeds);
+  useEffect(() => {
+    fetch("https://hackthenorth2022.uc.r.appspot.com/api/velocities")
+        .then(res => res.json())
+        .then(res => {
+          setData(res);
+
+          const times = [];
+          const speeds = [];
+          let _maxSpeed = 0;
+          let _averageSpeed = 0;
+          let count = 0;
+
+          for (const item of data) {
+            times.push(item.time);
+            speeds.push(item.value);
+            _maxSpeed = Math.max(_maxSpeed, item.value);
+            _averageSpeed = (_averageSpeed * count + item.value) / (count + 1);
+            count++;
+          }
+
+          setAverageSpeed(_averageSpeed);
+          setMaxSpeed(_maxSpeed);
+
+          setGraphData({
+            labels: times,
+            datasets: [
+              {
+                label: "speeds",
+                data: speeds,
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
+              },
+            ]
+          });
+        })
+  })
 
   return (
     <div className="dashboard">
@@ -51,17 +86,20 @@ function App() {
           <Line data={graphData} options={{ maintainAspectRatio: false }}/>
         </div>
 
-        <div className="velocity-display">
-          <h3>HIGHEST VELOCITY</h3>
-          <p><span>{maxSpeed}</span> m/s</p>
-        </div>
-        <div className="velocity-display">
-          <h3>AVERAGE VELOCITY</h3>
-          <p><span>{averageSpeed}</span> m/s</p>
+        <div>
+          <div className="velocity-display">
+            <h3>HIGHEST SPEED</h3>
+            <p><span>{maxSpeed}</span> m/s</p>
+          </div>
+          <div className="velocity-display">
+            <h3>AVERAGE SPEED</h3>
+            <p><span>{averageSpeed}</span> m/s</p>
+          </div>
         </div>
       </div>
     </div>
   );
+
 }
 
 export default App;
